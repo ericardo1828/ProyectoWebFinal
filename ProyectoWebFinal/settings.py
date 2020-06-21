b318a5f9351bbb16911d6263ac2ea950c61d74a6
LAPTOP-RG8ETG22\emunoz <ericardo1828@gmail.com>
2020-06-21 03:37:35 -0500

    Se me paso ponerle comillas simples al asterisco de allowed_hosts en el sarchivo settings.py

--- ProyectoWebFinal/settings.py
+++ ProyectoWebFinal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '70^&l03vd21ju34i-t&a7-^djr@n2*-l70c1ym16+-oxm8m+ki'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
